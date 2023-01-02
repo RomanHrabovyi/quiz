3c2c22e9f95946a546b4cd020e760b0276bf6e2a
RomanGrabovyi <60966944+RomanGrabovyi@users.noreply.github.com>
2023-01-02 23:30:52 +0200

    add quiz html

---
+++ src/App.js
@@ -0,0 +1,60 @@
+
+import './App.scss';
+
+const questions = [
+  {
+    title: 'React - це ... ?',
+    variants: ['бібліотека', 'фреймворк', 'додаток'],
+    correct: 0
+  },
+  {
+    title: 'Компонент - це ... ',
+    variants: ['додаток', 'частина додатку або сторінки', 'я незнаю що це'],
+    correct: 1,
+  },
+  {
+    title: 'Що таке JSX?',
+    variants: [
+      'Це простий HTML',
+      'Це функція',
+      'Це той же HTML, тільки з можливістю виконувати JS-код',
+    ],
+    correct: 2,
+  } 
+]
+
+const Result = () => {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="" />
+      <button>Спробувати ще</button>
+    </div>
+  )
+}
+
+const Game = () => {
+  return (
+    <>
+      <div className="progres">
+        <div className="progress__inner"></div>
+      </div>
+      <h1>Що...?</h1>
+      <ul>
+        <li>Відповіді</li>
+        <li>Відповіді</li>
+        <li>Відповіді</li>
+      </ul>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Game/>
+      <Result/>
+    </div>
+  );
+}
+
+export default App;
